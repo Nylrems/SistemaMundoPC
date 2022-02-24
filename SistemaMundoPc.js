f83395f7330e3d5d87aa0429aa37e97230c37dfa
Slermyn <smerlyn_alejandro@outlook.com>
2022-02-24 15:09:45 -0400

    Agregando clase de Raton

--- SistemaMundoPc.js
+++ SistemaMundoPc.js
@@ -14,7 +14,25 @@ class DispositivoEntrada{
     get marca(){
         return this._marca;
     }
-    set marca(){
+    set marca(marca){
         this._marca = marca;
     }
 }
+
+class Raton extends DispositivoEntrada{
+    static contadorRatones = 0;
+
+    constructor(tipoEntrada, marca){
+        super(tipoEntrada, marca);
+        this._idRaton = ++Raton.contadorRatones;
+    }
+    get idRaton(){
+        return this._idRaton;
+    }
+    toString(){
+        return `Ratón: [idRatón: ${this._idRaton}, tipoEntrada: ${this._tipoEntrada}, marca: ${this._marca}]`
+    }
+}
+
+let raton1 = new Raton('Usb', 'Logitech');
+console.log(raton1.toString());
